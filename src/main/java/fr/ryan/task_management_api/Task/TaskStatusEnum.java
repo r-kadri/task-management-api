9ecc51b3d7f4edeab96a8e39f1e0a3d19853b559
Ryan Kadri <ryankadri.dev@gmail.com>
2023-11-11 23:14:33 +0100

    :card_file_box: ADD: Task Status enumeration

---
+++ src/main/java/fr/ryan/task_management_api/Task/TaskStatusEnum.java
@@ -0,0 +1,17 @@
+package fr.ryan.task_management_api.Task;
+
+public enum TaskStatusEnum {
+    
+    IN_PROGRESS("In Progress"),
+    COMPLETED("Completed");
+
+    private final String status;
+
+    private TaskStatusEnum(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
